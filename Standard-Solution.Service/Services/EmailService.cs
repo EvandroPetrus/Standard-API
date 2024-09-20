6adbdb0b51cb458639917be9d880ae7517b71a9f
Evandro Ferraz <evandro.ferraz@infisconsultoria.com.br>
2024-09-20 19:58:18 -0300

    ✨ feat: Services without implementation, just so that it's possible to customize for each case

---
+++ Standard-Solution.Service/Services/EmailService.cs
@@ -0,0 +1,11 @@
+﻿using Standard_Solution.Domain.Interfaces.Services;
+
+namespace Standard_Solution.Service.Services;
+
+public class EmailService : IEmailService
+{
+    public void SendEmail(string userName, string email, string emailTemplatePath, string token, string subject)
+    {
+        throw new NotImplementedException();
+    }
+}
