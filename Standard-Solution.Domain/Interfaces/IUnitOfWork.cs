290cc0021bf96ca8eef561589531ec101e91ac17
Evandro Ferraz <evandro.ferraz@infisconsultoria.com.br>
2024-09-20 19:56:58 -0300

    ✨ feat: Models, Validator and IUnitOfWork interfacesp

---
+++ Standard-Solution.Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using Standard_Solution.Domain.Interfaces.Repositories;
+
+namespace Standard_Solution.Domain.Interfaces;
+public interface IUnitOfWork : IDisposable
+{
+    IUserRepository Users { get; }
+    int Complete();
+    Task CompleteAsync();
+}
---
+++ Standard-Solution.Domain/Models/User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Standard_Solution.Domain.Models
+{
+    public class User : IdentityUser
+    {
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    }
+}
