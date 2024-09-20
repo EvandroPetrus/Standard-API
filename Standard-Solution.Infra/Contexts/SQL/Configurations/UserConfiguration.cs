0f333120bf6ceea70df64d9f7b148bc1667c5c99
Evandro Ferraz <evandro.ferraz@infisconsultoria.com.br>
2024-09-20 19:59:29 -0300

    ✨ feat: Implementation of contexts and repositories, so that it compiles and let the person see what the API will look like

---
+++ Standard-Solution.Infra/Contexts/SQL/Configurations/UserConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Standard_Solution.Domain.Models;
+
+namespace Standard_Solution.Infra.Contexts.SQL.Configurations;
+
+internal class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable("Users");
+
+        builder.HasKey(u => u.Id);
+
+        builder.Property(u => u.CreatedAt).HasColumnType("datetime2").IsRequired();
+    }
+}
---
+++ Standard-Solution.Infra/Repositories/UsersRepository.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Standard_Solution.Domain.Interfaces.Repositories;
+using Standard_Solution.Domain.Models;
+        
+namespace Standard_Solution.Infra.Repositories;
+
+public class UsersRepository : IUserRepository
+{
+    private readonly UserManager<User> _userManager;
+
+    public UsersRepository(UserManager<User> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public async Task<IdentityResult> ChangePassword(User user, string token, string password)
+        => await _userManager.ChangePasswordAsync(user, token, password);
+
+    public async Task<bool> CheckUserPassword(User user, string password)
+        => await _userManager.CheckPasswordAsync(user, password);
+
+    public Task<IdentityResult> ConfirmUserEmail(User user, string token)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IdentityResult> EditUser(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string> GenerateTokenResetPassword(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string> GenerateTokenVerifyEmail(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> GetUserByEmail(string email)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> GetUserById(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IdentityResult> InsertUser(User user, string password)
+    {
+        throw new NotImplementedException();
+    }
+}
