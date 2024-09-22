5c8a2adfc3211837477acd6daf709ae85dc0b76e
Evandro Ferraz <evandro.ferraz@infisconsultoria.com.br>
2024-09-21 23:51:03 -0300

    ✨ feat: Adding the Interface of UOW, validators, configuration and the UOW itself

---
+++ Standard-Solution.Domain/Validator/SignInRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Standard_Solution.Domain.DTOs.Request;
+
+namespace Standard_Solution.Domain.Validator;
+public class SignInRequestValidator : AbstractValidator<UserLoginRequest>
+{
+    public SignInRequestValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required.")
+            .EmailAddress().WithMessage("Invalid email format.");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required.");
+    }
+}
