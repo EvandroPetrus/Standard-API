0b54f0246de4f8ffef8303afb3641a85236d2154
Evandro Ferraz <evandro.ferraz@infisconsultoria.com.br>
2024-09-21 23:51:37 -0300

    ✨ feat: Implementing Object mappers and NoSql contexts and setttings

---
+++ Standard-Solution.Infra/Contexts/NoSQL/MongoDbSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Standard_Solution.Infra.Contexts.NoSQL;
+
+public class MongoDbSettings
+{
+    public string Host { get; set; }
+    public string Name { get; set; }
+    public bool IsSSL { get; set; }
+
+}
