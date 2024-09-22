bc78c57ddeb05ed265e286e728443da886b4d998
Evandro Ferraz <evandro.ferraz@infisconsultoria.com.br>
2024-09-22 20:17:35 -0300

    ✨ feat: Added validation filter for incoming requests and it's DI

---
+++ Standard-Solution.API/Filter/ValidationFilter.cs
@@ -0,0 +1,47 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Standard_Solution.API.Filter;
+
+public class ValidationFilter : IAsyncActionFilter
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public ValidationFilter(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        if (!context.ModelState.IsValid)
+        {
+            context.Result = new BadRequestObjectResult(context.ModelState);
+            return;
+        }
+
+        var parameters = context.ActionDescriptor.Parameters;
+        foreach (var parameter in parameters)
+        {
+            var parameterValue = context.ActionArguments[parameter.Name];
+            if (parameterValue is null) continue;
+
+            var validatorType = typeof(IValidator<>).MakeGenericType(parameter.ParameterType);
+            var validator = _serviceProvider.GetService(validatorType) as IValidator;
+
+            if (validator is not null)
+            {
+                var validationContext = new ValidationContext<object>(parameterValue);
+                var validationResult = await validator.ValidateAsync(validationContext);
+
+                if (!validationResult.IsValid)
+                {
+                    context.Result = new BadRequestObjectResult(validationResult);
+                    return;
+                }
+            }
+        }
+        await next();
+    }
+}
